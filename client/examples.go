25e5b097d264058d92fc68120b547f235270efb9
knandha854 <nandhakumar.m@rently.com>
2025-02-23 14:34:50 +0530

    Added examples for client testing

--- client/examples.go
+++ client/examples.go
@@ -30,8 +30,67 @@ func main() {
         To:   "France",
         User: user,
     })
-    if err != nil {
+
+	if err != nil {
         log.Fatalf("PurchaseTicket failed: %v", err)
     }
     log.Printf("Purchased Ticket: %v", purchaseResp)
+
+	user1 := &proto.User{Email: "test23@example.com", FirstName: "Nandha", LastName: "Kumar"}
+    purchaseResp1, err := client.PurchaseTicket(context.Background(), &proto.PurchaseTicketRequest{
+        From: "London",
+        To:   "France",
+        User: user1,
+    })
+
+
+    if err != nil {
+        log.Fatalf("PurchaseTicket failed: %v", err)
+    }
+    log.Printf("Purchased Ticket: %v", purchaseResp1)
+
+	user2 := &proto.User{Email: "test3@example.com", FirstName: "Nandha", LastName: "Kumar"}
+    purchaseResp2, err := client.PurchaseTicket(context.Background(), &proto.PurchaseTicketRequest{
+        From: "London",
+        To:   "France",
+        User: user2,
+    })
+
+
+    if err != nil {			// Simple round-robin to assign seats
+
+        log.Fatalf("PurchaseTicket failed: %v", err)
+    }
+    log.Printf("Purchased Ticket: %v", purchaseResp2)
+
+	// Get Ticket
+	getResp, err := client.GetReceipt(context.Background(), &proto.GetReceiptRequest{ Email: user.Email })
+	if err != nil {
+		log.Fatalf("GetTicket failed: %v", err)
+	}
+	log.Printf("Get Ticket: %v", getResp)
+
+	// Get User by Section
+	userResp, err := client.GetUsersBySection(context.Background(), &proto.GetUsersBySectionRequest{ Section: "A" })
+	if err != nil {
+		log.Fatalf("GetUsersBySection failed: %v", err)
+	}
+	log.Printf("Get Users by Section: %v", userResp)
+
+	// Modify User Seat	
+	modifyResp, err := client.ModifyUserSeat(context.Background(), &proto.ModifyUserSeatRequest{
+		Email: user.Email,
+		NewSeat: &proto.Seat{SeatNumber: getResp.Seat.GetSeatNumber(), Section: getResp.Seat.GetSection()},
+	})
+	if err != nil {
+		log.Fatalf("ModifyUserSeat failed: %v", err)
+	}
+	log.Printf("Modify User Seat: %v", modifyResp)
+
+	// Remove User
+	removeResp, err := client.RemoveUser(context.Background(), &proto.RemoveUserRequest{ Email: user.Email })
+	if err != nil {
+		log.Fatalf("RemoveUser failed: %v", err)
+	}
+	log.Printf("Remove User: %v", removeResp)
 }
